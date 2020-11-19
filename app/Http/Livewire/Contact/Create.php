f84c74166f249d7dfd3b7ad8feb6fafb45b5bcc5
Vedat Bozkurt <info@wedat.org>
2020-11-19 16:36:32 +0300

    upload image

--- app/Http/Livewire/Contact/Create.php
+++ app/Http/Livewire/Contact/Create.php
@@ -4,17 +4,22 @@ namespace App\Http\Livewire\Contact;
 
 use Livewire\Component;
 use App\Models\Contact;
+use Livewire\WithFileUploads;
 
 class Create extends Component
 {
+    use WithFileUploads;
+
     public $name;
     public $phone;
     public $status;
+    public $photo;
 
     protected $rules = [
         'name' => 'required',
         'phone' => 'required|min:3|max:15',
         'status' => 'required',
+        'photo' => 'image|max:1024', // 1MB Max
     ];
 
     public function updated($propertyName)
@@ -25,10 +30,15 @@ class Create extends Component
     public function store()
     {
         $contact = $this->validate();
+
+        $photoname = md5($this->photo . microtime()).'.'.$this->photo->extension();
+        $this->photo->storeAs('photos', $photoname);
+
         $contact = Contact::create([
             'name' => $this->name,
             'phone' => $this->phone,
-            'status' => $this->status
+            'status' => $this->status,
+            'photo' => $photoname,
         ]);
         $this->resetInput();
         session()->flash('message', 'Contact ' . $contact['name'] . ' was created');
@@ -39,6 +49,7 @@ class Create extends Component
     {
         $this->name = null;
         $this->phone = null;
+        $this->photo = null;
         $this->status = null;
     }
 
--- resources/views/livewire/contact/create.blade.php
+++ resources/views/livewire/contact/create.blade.php
@@ -6,7 +6,7 @@
 <div class="p-10 mb-3 bg-white overflow-hidden shadow-xl sm:rounded-lg">
     <div class="flex flex-wrap -mx-3 mb-6">
         <form class="flex flex-wrap w-full" wire:submit.prevent="store">
-            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <div class="w-full md:w-1/4 px-3 mb-6 md:mb-0">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
                     Name
                 </label>
@@ -15,7 +15,7 @@
                     <p class="text-red-500 text-xs italic">{{ $message }}</p>
                 @enderror
             </div>
-            <div class="w-full md:w-1/3 px-3">
+            <div class="w-full md:w-1/4 px-3">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
                     Phone
                 </label>
@@ -24,7 +24,7 @@
                     <p class="text-red-500 text-xs italic">{{ $message }}</p>
                 @enderror
             </div>
-            <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <div class="w-full md:w-1/4 px-3 mb-6 md:mb-0">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
                     Status
                 </label>
@@ -36,6 +36,16 @@
                 @error('status')
                     <p class="text-red-500 text-xs italic">{{ $message }}</p>
                 @enderror
+            </div>
+            <div class="w-full md:w-1/4 px-3">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+                    Photo
+                </label>
+                <input wire:model="photo" class="appearance-none block w-full bg-gray-200 text-gray-700 border @error('photo') border-red-500 @enderror rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-300" type="file">
+                @error('photo')
+                    <p class="text-red-500 text-xs italic">{{ $message }}</p>
+                @enderror
+                {{-- <div wire:loading wire:target="photo" class="text-sm text-gray-500 italic">Uploading...</div> --}}
             </div>
             <div class="flex items-center ml-3 mt-4">
                 <button type="submit" class="inline-flex items-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
